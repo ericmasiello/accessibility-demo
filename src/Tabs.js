9f7afe387987574aab56ea3039cb63c4e447ecda
Eric Masiello <eric.j.masiello@gmail.com>
2018-11-18 13:20:59 -0500

    Working POC

--- src/Tabs.js
+++ src/Tabs.js
@@ -2,33 +2,79 @@ import React from 'react';
 import classNames from 'classnames';
 import diamonddogs from './data/diamonddogs';
 import './Tabs.css';
+import './Bio.css';
+
+const KEYS = {
+  right: 39,
+  left: 37,
+  down: 40,
+};
+
+const findNext = (tabIds, selectedId) => {
+  const index = tabIds.findIndex(id => id === selectedId);
+  if (index === -1) {
+    return selectedId;
+  }
+
+  if (index === tabIds.length - 1) {
+    return tabIds[0];
+  }
+
+  return tabIds[index + 1];
+};
+
+const findPrevious = (tabIds, selectedId) => {
+  const index = tabIds.findIndex(id => id === selectedId);
+  if (index === -1) {
+    return selectedId;
+  }
+
+  if (index === 0) {
+    return tabIds[tabIds.length - 1];
+  }
+
+  return tabIds[index - 1];
+};
 
 class TabManager extends React.Component {
   handleSetSelected = id => this.setState({ selected: id });
   handleKeyDown = event => {
-    debugger;
+    switch (event.which) {
+      case KEYS.right: {
+        this.setState(
+          {
+            selected: findNext(this.props.tabIds, this.state.selected),
+          },
+          () => {
+            this.state.selectedTabRef.current.focus();
+          },
+        );
+        break;
+      }
+      case KEYS.left: {
+        this.setState(
+          {
+            selected: findPrevious(this.props.tabIds, this.state.selected),
+          },
+          () => {
+            this.state.selectedTabRef.current.focus();
+          },
+        );
+        break;
+      }
+      case KEYS.down: {
+        this.state.panelRef.current.focus();
+        break;
+      }
+    }
   };
+
   state = {
-    selected: this.props.selected,
+    selected: this.props.tabIds ? this.props.tabIds[0] : null,
     setSelected: this.handleSetSelected,
-    tabProps: {
-      role: 'tab',
-      onKeyDown: this.handleKeyDown,
-      tabIndex: -1,
-    },
-    selectedTabProps: {
-      role: 'tab',
-      onKeyDown: this.handleKeyDown,
-      'aria-selected': 'true',
-    },
-    panelProps: {
-      role: 'tabpanel',
-      hidden: true,
-    },
-    selectedPanelProps: {
-      tabIndex: 0,
-      role: 'tabpanel',
-    },
+    handleTabKeyDown: this.handleKeyDown,
+    panelRef: React.createRef(),
+    selectedTabRef: React.createRef(),
   };
 
   render() {
@@ -36,61 +82,68 @@ class TabManager extends React.Component {
   }
 }
 
-export default class Tabs extends React.Component {
+export default class Tabs extends React.PureComponent {
   render() {
     return (
-      <TabManager selected={diamonddogs[0].id}>
-        {tabState => (
-          <>
-            <div role="tablist" className="tablist">
-              {diamonddogs.map(member => {
-                const tabProps =
-                  tabState.selected === member.id
-                    ? tabState.selectedTabProps
-                    : tabState.tabProps;
-
-                return (
-                  <a
+      <TabManager tabIds={diamonddogs.map(member => member.id)}>
+        {tabState => {
+          const selectedMember = diamonddogs.find(
+            member => member.id === tabState.selected,
+          );
+          return (
+            <>
+              <h1 className="page-title">Diamond Dogs</h1>
+              <div role="tablist" className="tablist" aria-label="Team members">
+                {diamonddogs.map(member => (
+                  <button
                     key={member.id}
-                    {...tabProps}
+                    id={`tab_${member.id}`}
+                    role="tab"
+                    aria-selected={tabState.selected === member.id}
+                    aria-controls={
+                      tabState.selected === member.id
+                        ? `${selectedMember.id}_panel`
+                        : undefined
+                    }
+                    tabIndex={tabState.selected !== member.id ? -1 : undefined}
+                    ref={
+                      tabState.selected === member.id && tabState.selectedTabRef
+                    }
+                    onKeyDown={tabState.handleTabKeyDown}
+                    onClick={() => {
+                      tabState.setSelected(member.id);
+                    }}
                     className={classNames([
                       'tab',
                       {
                         'tab--selected': tabState.selected === member.id,
                       },
                     ])}
-                    href={`#${member.id}_panel`}
-                    id={`#${member.id}_tab`}
-                    onClick={event => {
-                      event.preventDefault();
-                      tabState.setSelected(member.id);
-                    }}
                   >
                     {member.name}
-                  </a>
-                );
-              })}
-            </div>
-            {diamonddogs.map(member => {
-              const panelProps =
-                tabState.selected === member.id
-                  ? tabState.selectedPanelProps
-                  : tabState.panelProps;
-
-              return (
+                  </button>
+                ))}
+              </div>
+              {selectedMember && (
                 <section
-                  key={member.id}
-                  {...panelProps}
-                  id={`#${member.id}_panel`}
-                  aria-labelledby={`#${member.id}_tab`}
+                  ref={tabState.panelRef}
+                  role="tabpanel"
+                  className="bio"
+                  tabIndex={0}
+                  id={`${selectedMember.id}_panel`}
+                  aria-labelledby={`tab_${selectedMember.id}`}
                 >
-                  <img src={member.photo} />
-                  {member.bio}
+                  <img
+                    className="bio__photo"
+                    src={selectedMember.photo}
+                    alt={selectedMember.photoDesc}
+                  />
+                  <p>{selectedMember.bio}</p>
                 </section>
-              );
-            })}
-          </>
-        )}
+              )}
+            </>
+          );
+        }}
       </TabManager>
     );
   }
