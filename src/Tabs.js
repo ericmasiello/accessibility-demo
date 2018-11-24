6c71f5287314049f2d56659e4652093846b07ba3
Eric Masiello <eric.j.masiello@gmail.com>
2018-11-24 08:44:40 -0500

    Add TabManager for all keyboard AX

--- src/Tabs.js
+++ src/Tabs.js
@@ -4,51 +4,141 @@ import diamonddogs from './data/diamonddogs';
 import './Tabs.css';
 import './Bio.css';
 
-export default class Tabs extends React.PureComponent {
+const KEYS = {
+  right: 39,
+  left: 37,
+  down: 40,
+};
+
+const findNext = (tabIds, selectedId) => {
+  const index = tabIds.findIndex(id => id === selectedId);
+  if (index === -1) {
+    return selectedId;
+  }
+
+  if (index === tabIds.length - 1) {
+    return tabIds[0];
+  }
+
+  return tabIds[index + 1];
+};
+
+const findPrevious = (tabIds, selectedId) => {
+  const index = tabIds.findIndex(id => id === selectedId);
+  if (index === -1) {
+    return selectedId;
+  }
+
+  if (index === 0) {
+    return tabIds[tabIds.length - 1];
+  }
+
+  return tabIds[index - 1];
+};
+
+class TabManager extends React.Component {
+  handleSetSelected = id => this.setState({ selected: id });
+  handleKeyDown = event => {
+    switch (event.which) {
+      case KEYS.right: {
+        this.setState(
+          {
+            selected: findNext(this.props.tabIds, this.state.selected),
+          },
+          () => {
+            this.state.selectedTabRef.current.focus();
+          },
+        );
+        break;
+      }
+      case KEYS.left: {
+        this.setState(
+          {
+            selected: findPrevious(this.props.tabIds, this.state.selected),
+          },
+          () => {
+            this.state.selectedTabRef.current.focus();
+          },
+        );
+        break;
+      }
+      case KEYS.down: {
+        this.state.panelRef.current.focus();
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  };
+
   state = {
-    selected: diamonddogs.map(member => member.id)[0],
+    selected: this.props.tabIds ? this.props.tabIds[0] : null,
+    setSelected: this.handleSetSelected,
+    handleTabKeyDown: this.handleKeyDown,
+    panelRef: React.createRef(),
+    selectedTabRef: React.createRef(),
   };
-  setSelected = id => {
-    this.setState({ selected: id });
-  };
+
   render() {
-    const selectedMember = diamonddogs.find(
-      member => member.id === this.state.selected,
-    );
+    return this.props.children(this.state);
+  }
+}
+
+export default class Tabs extends React.PureComponent {
+  render() {
     return (
-      <>
-        <div className="page-title">Diamond Dogs</div>
-        <div className="tablist">
-          {diamonddogs.map(member => (
-            <button
-              key={member.id}
-              onClick={() => {
-                this.setSelected(member.id);
-              }}
-              className={classNames([
-                'tab',
-                {
-                  'tab--selected': this.state.selected === member.id,
-                },
-              ])}
-            >
-              {member.name}
-            </button>
-          ))}
-        </div>
-        {selectedMember && (
-          <section>
-            <img className="bio-photo" src={selectedMember.photo} alt="" />
-            <div className="bio">
-              <div className="bio__label">Hometown</div>
-              <div className="bio__item">{selectedMember.hometown}</div>
-              <div className="bio__label">Time at Vistaprint</div>
-              <div className="bio__item">{selectedMember.employment}</div>
-              <div className="bio__item">{selectedMember.bio}</div>
-            </div>
-          </section>
-        )}
-      </>
+      <TabManager tabIds={diamonddogs.map(member => member.id)}>
+        {tabState => {
+          const selectedMember = diamonddogs.find(
+            member => member.id === tabState.selected,
+          );
+          return (
+            <>
+              <div className="page-title">Diamond Dogs</div>
+              <div className="tablist">
+                {diamonddogs.map(member => (
+                  <button
+                    key={member.id}
+                    tabIndex={tabState.selected !== member.id ? -1 : undefined}
+                    ref={
+                      tabState.selected === member.id && tabState.selectedTabRef
+                    }
+                    onKeyDown={tabState.handleTabKeyDown}
+                    onClick={() => {
+                      tabState.setSelected(member.id);
+                    }}
+                    className={classNames([
+                      'tab',
+                      {
+                        'tab--selected': tabState.selected === member.id,
+                      },
+                    ])}
+                  >
+                    {member.name}
+                  </button>
+                ))}
+              </div>
+              {selectedMember && (
+                <section ref={tabState.panelRef} tabIndex={0}>
+                  <img
+                    className="bio-photo"
+                    src={selectedMember.photo}
+                    alt=""
+                  />
+                  <div className="bio">
+                    <div className="bio__label">Hometown</div>
+                    <div className="bio__item">{selectedMember.hometown}</div>
+                    <div className="bio__label">Time at Vistaprint</div>
+                    <div className="bio__item">{selectedMember.employment}</div>
+                    <div className="bio__item">{selectedMember.bio}</div>
+                  </div>
+                </section>
+              )}
+            </>
+          );
+        }}
+      </TabManager>
     );
   }
 }
