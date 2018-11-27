40ebc239a2d486310b37bb330992cee058ede2ff
Eric Masiello <eric.j.masiello@gmail.com>
2018-11-26 20:48:15 -0500

    Add default overlay state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import queryString from 'query-string';
+import classNames from 'classnames';
 import Tabs from './Tabs';
 import './App.css';
 
-
 class App extends Component {
+  state = {
+    overlay: queryString.parse(window.location.search).overlay !== 'false',
+  };
   render() {
+    const classes = [
+      'App',
+      {
+        'App--overlay': this.state.overlay,
+      },
+    ];
     return (
-      <div className="App">
+      <div className={classNames(classes)}>
         <Tabs />
+        {this.state.overlay && (
+          <div className="overlay">
+            <button
+              className="overlay__button"
+              onClick={() =>
+                this.setState({
+                  overlay: false,
+                })
+              }
+            >
+              Reveal
+            </button>
+          </div>
+        )}
       </div>
     );
   }
